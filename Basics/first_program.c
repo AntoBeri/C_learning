4531256a4bd616efbc89031c0d9cfdef4421785a
Beri <antonio.beri.grc@gmail.com>
2025-10-27 23:00:37 -0600

    Added commentary about the compiling process in a C program

--- Basics/first_program.c
+++ Basics/first_program.c
@@ -1,5 +1,14 @@
 //This program prints "Hello World!" to the console window
-#include <stdio.h>
+
+//.h are header files
+#include <stdio.h> //Preprocessor directive
+//Preprocessor replaces the text starting with # with the file content before 
+//the compilation begins.
+//The headers are not the libraries itself but a declaration of what exists in the
+//library, and the C standard library is where the actual definitions of 
+//functions are.
+//A program called linker maps the prototypes in the header to the actual code in the
+// C standard library.
 
 int main()
 {
